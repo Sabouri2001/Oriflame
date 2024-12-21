a9c649e1b066c0ef70d331a0b09d6eb5fd3dda10
Fatemeh Sabouri <fatemehsabouri@Fatemehs-MacBook-Pro.local>
2024-12-21 12:55:29 +0330

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./**/*.html",
+    "./public/**/*.{js,css}",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: '600px',
+         md: '900px',
+        xl: '1200px'
+       
+      }
+    },
+  },
+  plugins: [],
+}
+
